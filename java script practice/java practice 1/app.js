7b191c761fadcfbc8f3ee7c1a6755d9b47f7f347
bilalkhattak55 <mbilalkhk@gmail.com>
2021-06-23 23:55:59 +0500

    java script practice

---
+++ java script practice/java practice 1/app.js
@@ -0,0 +1,202 @@
+// var a = 5;
+// var b = 10;
+// var c = a+b;
+// alert(c);
+// 
+
+// var $ali = 2;
+// var bilal = 5;
+// var umer = "ali" + "bilal";
+// alert(umer);
+// var f = 10;
+// var g = "20";
+// var h = g/f;
+// alert(h);
+// var a = 3;
+// var b = 5;
+// var c = 7;
+// var d = b-a*c;
+// alert(d);
+// 
+
+// var a = 5;
+// var b = a++;
+// alert (a);
+
+// var a = 5;
+// var b = ++a;
+// alert(b);
+
+// var a = 5;
+// var b = --a;
+// alert(b);
+
+// var a = 5;
+// var b = a--;
+// alert(a);
+
+// var x = 10;
+// var y = x++ + ++x -x + x++ - ++x + x;
+// alert(y);
+// alert(x);
+
+// var a = prompt ("Enter your age","Your age is");
+// alert(a);
+
+// var a = prompt("Enter your value","Your value is");
+// var b = a + "10";
+// console.log(b);
+// var a = prompt ("Enter your value","Your value is");
+// var b = a + 10;
+// console.log("the output is", " " + b)
+// var a = +prompt("Enter your value","your value is");
+// var b = a + 10;
+// console.log("the output is", " " + b);
+
+
+// var x = "Enter your value";
+// var y = "Your value is";
+// var a = +prompt(x,y);
+// var b = a + 10;
+// console.log("the output is", + "" + b);
+
+// var x = "enter your value";
+// var y = "your value is";
+// var a = +prompt(x,y)
+// var b = a + 10;
+// document.write("the output is : ", + " " + b)
+
+
+
+
+// 1=====conditional statement....
+
+
+// var age = +prompt("Enter your age");
+// if (age == 14){
+//     alert("age is Equal.")
+// }
+// else if(age < 14){
+//     alert("age is less:")
+// }
+// else{
+//     alert("your age is more")
+// }
+
+
+// var age = +prompt ("Enter your age");
+// var gender = prompt("Enter your gender");
+// if(age > 18 && gender == "male"){
+//     alert("you are allowed to ride")
+// }
+// else{
+//     alert("you are not allowed")
+// }
+
+// var age = +prompt ("Enter your age");
+// var gender = prompt ("Enter your gender");
+// if(age > 18 || gender == "male"){
+//     alert("you are allowed")
+// }
+// else{
+//     alert("you are not allowed")
+// }
+
+// var a = "5"
+// if(a == 5){
+//     alert("true")
+// }
+// else{
+//     alert("false")
+// }
+// var a ="5";
+// if(a === 5){
+//     alert("true")
+// }
+// else{
+//     alert("false")
+// }
+
+
+// var x = 5;
+// var y = 10;
+// var a = 30;
+// var b = 15;
+// if(x-y === a-b){
+//     alert("correct")
+// }
+// else{
+//     alert("incorrect")
+// }
+// var x = 5;
+// var y = 10;
+// var a = 30;
+// var b = 15;
+// if(x+y === a-b){
+//     alert("correct")
+// }
+// else{
+//     alert("incorrect")
+// }
+
+
+// var a = 10;
+// if (a !== 15){
+//     document.write("correct")
+// }
+// else{
+//     document.write("incorrect")
+// }
+
+// var percent = +prompt ("Enter your percentage")
+// if(percent >= 80 && percent <= 100){
+//     alert("A+")
+// }
+// else if (percent >= 70 && percent <= 80){
+//     alert("A")
+// }
+// else if (percent >= 60 && percent <= 70){
+//     alert("B")
+// }
+// else if (percent >= 50 && percent <= 60){
+//     alert("C")
+// }
+// else if (percent >= 40 && percent <= 50){
+//     alert("D")
+// }
+// else if (percent >= 33 && percent <= 50){
+//     alert("E")
+// }
+// else{
+//     alert("you did not write correct percent")
+// }
+
+// var students = ["ali" , "bilal" , "umer" , "hassan"];
+// document.write(students);
+
+// console.log(students[1]);
+
+// alert("wellcome" + students[3])
+// var names = [];
+
+// names [0] = "ali"
+// names [1] = "bilal"
+// names [2] = "umer"
+// names [3] = "hassan"
+
+// console.log(names);
+
+// var students = ["ghous","basit","ali"];
+// students.pop();
+// students.push("bilal","zunaib")
+
+// console.log(students);
+//  var students = ["ali","bilal","umer","hassan"];
+// //  students.shift();
+// //  students.unshift("ghous","zunaib")
+// students.splice(1,1 +"aasif","rashid")
+//  console.log(students)
+// var students =["ali","bilal","umer","hassan","zubair"];
+// console.log(students);
+// var copyStudents = students.slice(1,4);
+// console.log(copyStudents);
