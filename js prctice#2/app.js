02e0ea67da3af6732e4bd116f05631c9b54ffcda
bilalkhattak55 <76492345+bilalkhattak55@users.noreply.github.com>
2021-06-29 23:21:45 +0500

    Add files via upload

---
+++ js prctice#2/app.js
@@ -0,0 +1,35 @@
+
+// var name = "BILAL HAIDER"
+// name = name.toLowerCase();
+// console.log(name)
+// var name = "bilal haider"
+// name = name.toUpperCase();
+// console.log(name)
+
+// var name = "bilal Haider"
+// console.log(name.indexOf("H"))
+
+
+// var val1 = prompt("enter your first value");
+// var sign = prompt("enter your operater");
+// var val2 = prompt("enter your second value");
+
+// console.log(val1 + sign + val2)
+// if(sign === "+"){
+//     alert((+val1) + (+val2))
+// }else if(sign === "-"){
+//     alert(val1 - val2)
+// }else if(sign === "*"){
+//     alert(val1 * val2)
+// }else if(sign ==="/"){
+//     alert(val1 / val2)
+// }else if(sign === "%"){
+//     alert(val1 / val2 * 100)
+// }
+// var equ = prompt("enter your equation");
+// var num = equ.length
+// for (var i = 0; i < num; i++) {
+//     if (equ.slice(i,i+1) === "+" || equ.slice(i,i+1) === "-" || equ.slice(i,i+1) === "*" || equ.slice(i,i+1) === "/") {
+//         alert("sign has found")
+//     }
+// }
