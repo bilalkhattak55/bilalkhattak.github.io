68844ae31374343f4493008bd8c70c0b2b335c6c
bilalkhattak55 <76492345+bilalkhattak55@users.noreply.github.com>
2021-07-16 06:13:49 +0500

    Add files via upload

---
+++ js practice #5/app.js
@@ -0,0 +1,56 @@
+
+
+// *************** while loop****************
+
+// for(a = 0; a <= 10; a++){
+//     console.log(a)
+// }
+// var b = 0;
+// while(b <= 10){
+//     console.log(b);
+//     b++;
+// }
+// var c =0;
+// do{
+//     console.log(c)
+//     c++;
+// }
+// while(c <= 10);
+// function foo(){
+//     alert("hello world");
+// }
+// function foo(){
+
+
+
+//     var a = 2+2;
+//     alert(a);
+// }
+// function foo(greet){
+//     alert(greet)
+// }
+// function clickbtn(){
+//     alert("click")
+// }
+// function getName(){
+//     var name = document.getElementById("name")
+//     alert(name.value)
+// }
+// function getName(){
+//     var name = document.getElementById("bilal")
+//     console.log(name.value)
+//     name.value = ""
+// }
+function getName() {
+    var name = document.getElementById("xyz");
+    name.value = "bilal"
+    console.log(getName.value)
+    var para = document.getElementById("para");
+    para.innerHTML = "hello this is example"
+}
+getName();
+
+
+
+
+
