63b4c86dfbb1cfbd58269d825c999c047337747c
bilalkhattak55 <76492345+bilalkhattak55@users.noreply.github.com>
2021-07-16 06:17:56 +0500

    Add files via upload

---
+++ js practice#10 class#16.1/app.js
@@ -0,0 +1,34 @@
+// function getValue(){
+//     alert('test')
+// }
+
+// function setValue(){
+//     var name = document.getElementById('name')
+//     name.value='bilal'
+// }
+
+// function setValue(){
+//     var para = document.getElementById("para")
+//     para.innerHTML="this is example text."
+// }
+// function bigImage(){
+//     var image = document.getElementById('img')
+//     image.className="big"
+// }
+// function smallImage(){
+//     var image = document.getElementById('img')
+//     image.className="small"
+// }
+// function applyStyle(){
+//     var para = document.getElementById('para')
+//     para.className = "bold"
+// }
+// function applyStyle(){
+//     var para = document.getElementById('para');
+//     para.className += ' bold blue'
+// }
+// function getElements(){
+//     var element = document.getElementsByTagName('p')
+//     console.log(element)
+// }
+// getElements()
