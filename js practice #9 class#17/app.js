20418a0c71780d6e55d0be4ae3f1bb68c5db6845
bilalkhattak55 <76492345+bilalkhattak55@users.noreply.github.com>
2021-07-16 06:17:06 +0500

    Add files via upload

---
+++ js practice #9 class#17/app.js
@@ -0,0 +1,9 @@
+// function timer(){
+//     console.log("running...")
+// }
+// setInterval(timer , 1000)
+
+// console.log(document.childNodes[1].childNodes[1].childNodes[1].childNodes[1])
+
+var a = document.getElementById("papa");
+console.log(a.childNodes)
