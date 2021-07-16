f67cc1f50d9b7b34734e48f1006e272f1559e83f
bilalkhattak55 <76492345+bilalkhattak55@users.noreply.github.com>
2021-07-16 06:16:28 +0500

    Add files via upload

---
+++ js practice #8/app.js
@@ -0,0 +1,76 @@
+// function readMore(){
+//     var text = "adipisicing elit. Itaque, quas tempore? Magni, neque vero eaque nesciunt nihil praesentium corporis sit impedit dolorum illum alias quaerat ex, distinctio, saepe eveniet accusamus!"
+//     var paragraph = document.getElementById("para");
+//     paragraph.innerHTML = text
+
+
+
+// function readMore1(){
+//     var text1="my name is ali Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias officiis provident consequatur saepe iustosed fuga animi alias eius modi laboriosam beatae explicabo a sunt eligendi ex dolorem voluptate aperiam!"
+//     var paragraph1 = document.getElementById("para1")
+//     paragraph1.innerHTML = text1
+
+// } 
+// function readMore(){
+//     var text = document.getElementById("text").innerHTML;
+//     var paragraph = document.getElementById("para")
+//     paragraph.innerHTML = text
+// }
+
+
+// function big(){
+//     var img = document.getElementById("apple");
+//     img.className = "hidden"
+// }
+// function hide(){
+//     var pic = document.getElementById("apple");
+//     pic.className = "hide"
+// }
+// function hide(){
+//     var img = document.getElementById("apple");
+//     img.className += " hide"
+// }
+// function changeImage(){
+//     var image = document.getElementById('img')
+//     image.src = 'imgg.png'
+// }
+
+
+
+// function changeImage(){
+//     var carImage = document.getElementById('img')
+//     carImage.src = './images/imgg.png'
+// }
+// function changeImageBefore(){
+//     var image = document.getElementById('img')
+//     image.src="./images/download.jpg"
+// }
+
+
+
+// function changeImage(id,src){
+//     var image = document.getElementById(id);
+//     image.src = src
+// }
+// function changeImage(id,src){
+//     var image = document.getElementById(id);
+//     image.src = src
+// }
+// function changeImage(){
+//     var image = document.getElementById('img')
+//     image.style.width ='800px'
+// }
+// function changeSize(){
+//     var font = document.getElementById('para')
+//     font.style.fontSize='2em'
+// }
+// var para = document.getElementsByTagName('p');
+// para[2].style.color = 'red'
+// for (var i = 0; i < para.length; i++){
+//     para[i].style.color='blue'
+// }
+// var parent = document.getElementById('parent')
+// var para = parent.getElementsByTagName('p');
+// for(var i = 0; i < para.length; i++){
+//     para[i].style.color = 'blue'
+// }
