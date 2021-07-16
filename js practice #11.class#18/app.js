cf3c505705555f3485595cbfcfae9894a1682006
bilalkhattak55 <76492345+bilalkhattak55@users.noreply.github.com>
2021-07-16 06:18:24 +0500

    Add files via upload

---
+++ js practice #11.class#18/app.js
@@ -0,0 +1,53 @@
+// function disableBtn(){
+//     var btn = document.getElementById('example')
+//     btn.disabled = true
+// }
+// 
+// function getElements(){
+//     var p = document.getElementsByTagName('p')
+//     console.log(p[0])
+// }
+// function getElements(){
+//     console.log(document.childNodes[1])
+// }
+// var list = document.getElementsByTagName('li');
+// console.log(list.length)
+
+// var target =document.getElementById('p1');
+// if(target.hasAttribute('class')){
+//     alert('available')
+// }else{
+//     alert('not available')
+// }
+// console.log(target.getAttribute('class'))
+
+// var target=document.getElementById('p1');
+// target.setAttribute('class','blue')
+// // console.log(target.getAttribute('class'))
+// console.log(target.attributes[1].nodeName)
+
+
+// var p = document.createElement('p');
+// var text = document.createTextNode('hello world')
+// p.appendChild(text)
+
+// var main = document.getElementById('main');
+// main.appendChild(p)
+// console.log(p)
+// var p =document.createElement('h1');
+// var text = document.createTextNode('my name is M Bilal.')
+// p.appendChild(text)
+// console.log(p)
+// var main = document.getElementById('main')
+// main.appendChild(p)
+
+
+// function addMessage(){
+//     var val = document.getElementById('val')
+//     var p = document.createElement('p')
+//     var textNode = document.createTextNode(val.value)
+//     p.appendChild(textNode)
+//     var message = document.getElementById('messages')
+//     message.appendChild(p)
+//     val.value=''
+// }
