ac5213e1b8db62d8b8943da412483b193841113c
bilalkhattak55 <76492345+bilalkhattak55@users.noreply.github.com>
2021-08-13 00:49:19 +0500

    Add files via upload

---
+++ js practice #12.class#19 and 20/app.js
@@ -0,0 +1,21 @@
+// var myQuestions = [
+//     {
+//         question : "what is your name",
+//         answer : {
+//             a : "ali",
+//             b : "bilal",
+//             c : "umer"
+//         },
+//         correctAnswer : "c"
+//     },
+//     {
+//         question : "what is your age",
+//         answer : {
+//             a : "12",
+//             b : "16",
+//             c : "18"
+//         },
+//         correctAnswer : "16"
+//     },
+// ]
+// console.log(myQuestions)
