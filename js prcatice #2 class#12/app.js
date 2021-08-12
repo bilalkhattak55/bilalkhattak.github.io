ed68d9701e97f5fdd28a36fbbf197c9d79dd2072
bilalkhattak55 <76492345+bilalkhattak55@users.noreply.github.com>
2021-08-13 00:48:05 +0500

    Add files via upload

---
+++ js prcatice #2 class#12/app.js
@@ -0,0 +1,62 @@
+// var arr = ["ali","bilal","umer","hassan"];
+// console.log(arr[3]);
+
+// var arr1 = new Array("tiger","lion","cat");
+// console.log(arr1);
+
+// arr[4]= "Dabbu";
+// console.log(arr)
+// arr.splice(2,2, "haider");
+// console.log(arr)
+
+// for (var i = 0; i < 5; i++){
+//     alert("hello world");
+// 
+
+// document.write("2" + "X" + "1" + "=" + 2 * 1 + "<br>") 
+// document.write("2" + "X" + "2" + "=" + 2 * 2 + "<br>")
+// document.write("2" + "X" + "3" + "=" + 2 * 3 + "<br>")
+// document.write("2" + "X" + "4" + "=" + 2 * 4 + "<br>")
+// document.write("2" + "X" + "5" + "=" + 2 * 5 + "<br>")
+// document.write("2" + "X" + "6" + "=" + 2 * 6 + "<br>")
+
+for(var i = 1; i <=10; i++){
+  document.write("2" + "X" + i + "=" + 2 * i + "<br>")
+}
+
+
+// var name = "BILAL HAIDER"
+// name = name.toLowerCase();
+// console.log(name)
+
+// var name = "bilal haider"
+// name = name.toUpperCase();
+// console.log(name)
+
+// var name = "bilal Haider"
+// console.log(name.indexOf("H"))
+
+
+// var val1 = prompt("enter your first value");
+// var sign = prompt("enter your operater");
+// var val2 = prompt("enter your second value");
+
+// console.log(val1 + sign + val2)
+// if(sign === "+"){
+//     alert((+val1) + (+val2))
+// }else if(sign === "-"){
+//     alert(val1 - val2)
+// }else if(sign === "*"){
+//     alert(val1 * val2)
+// }else if(sign ==="/"){
+//     alert(val1 / val2)
+// }else if(sign === "%"){
+//     alert(val1 / val2 * 100)
+// }
+// var equ = prompt("enter your equation");
+// var num = equ.length
+// for (var i = 0; i < num; i++) {
+//     if (equ.slice(i,i+1) === "+" || equ.slice(i,i+1) === "-" || equ.slice(i,i+1) === "*" || equ.slice(i,i+1) === "/") {
+//         alert("sign has found")
+//     }
+// }
