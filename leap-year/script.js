8e344898d30fd4a8e27b69746093f3d3962414fe
Muhammad Bilal <76492345+bilalkhattak55@users.noreply.github.com>
2022-09-10 21:46:36 +0500

    10, september 22 intermediate javascript
    random number, leap year, random array,etc

---
+++ leap-year/script.js
@@ -0,0 +1,97 @@
+// // Leap year
+// function isLeapYear(year) {
+//     if (year % 4 === 0) {
+//         if (year % 100 === 0) {
+//             if ( year % 400 === 0) {
+//                 return "leap year"
+//             }else {
+//                 return "not leap year"
+//             }
+//         }else {
+//             return "leap year"
+//         }
+//     }else {
+//         return "not a leap year"
+//     }
+       
+// }
+// isLeapYear(2032)
+
+
+
+
+
+// Arrays
+// var guests = ["bilal", "Ali", "Zaid", "Aziz", "Muzzamil"];
+// var guestName = prompt("write the name of guest")
+// if(guests.includes(guestName)) {
+//     alert("wellcome")
+// }else {
+//     alert("next time")
+// }
+
+
+
+
+
+// var output = [];
+// var count = 1;
+
+//  function fizzBuzz () {
+//     while(count <= 100) {
+//      if (count % 3 === 0 && count % 5 === 0) {
+//          output.push("fizzBuzz")
+//      }
+//      else if(count % 3 === 0) {
+//          output.push("fizz")  
+//      }else if (count % 5 === 0){
+//          output.push("buzz")
+//      }
+//      else {
+//          output.push(count);
+//      }
+     
+//      count++;
+   
+
+//      console.log(output)
+//     }
+//  }
+//  fizzBuzz ()
+
+
+
+
+
+
+
+
+ // var random = Math.floor(Math.random()*array.length);
+// console.log(array[random])
+
+// var r = Math.floor(Math.random()*5)
+// console.log(r)
+
+// var name = prompt("enter name");
+
+// var array = ["Ali", "umer", "Zaid"];
+// function whoPay(array) {
+//     var random = Math.floor(Math.random()*array.length);
+//     var whospay = array[random]
+//     console.log(whospay)
+//     return whospay + " is going to pay"
+// }
+// whoPay()
+
+
+
+
+
+// var array = ["Ali", "umer", "Zaid"];
+// function whoPay(array) {
+//     var numberOfPeople = array.length;
+//     var randomPersonPosition = Math.floor(Math.random()*numberOfPeople);
+//     var randomPerson = array[randomPersonPosition]
+//     return randomPerson + " is going to pay"
+    
+// }
